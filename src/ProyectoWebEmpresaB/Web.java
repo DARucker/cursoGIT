0904cba84e891fd8eaa46841af2ad7ccc688ee2b
Dario <dariomsn@yahoo.com.ar>
2021-06-21 12:15:09 -0300

    pide nombre e imprime

--- src/ProyectoWebEmpresaB/Web.java
+++ src/ProyectoWebEmpresaB/Web.java
@@ -2,9 +2,15 @@ package ProyectoWebEmpresaB;
 
 public class Web {
 
+
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebMain wm = new WebMain();
+		
+		String imprimir = wm.pedirNombre();
+		System.out.println("su nombre es: " + imprimir);
 	}
 
 }
--- src/ProyectoWebEmpresaB/WebMain.java
+++ src/ProyectoWebEmpresaB/WebMain.java
@@ -1,5 +1,16 @@
 package ProyectoWebEmpresaB;
 
+import java.util.Scanner;
+
 public class WebMain {
 
+	public String pedirNombre() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Indique su nombre: ");
+		String Nombre = sc.nextLine();
+		return Nombre;
+		
+	}
+	
 }
